5956531f0affba558c4225ef8b852613dfba7deb
Flako <54653591+Daniells-cmu@users.noreply.github.com>
2021-10-27 01:30:38 -0400

    Added exel catalog plus files

--- POS System/Main.py
+++ POS System/Main.py
@@ -2,6 +2,65 @@ from cmu_112_graphics import *
 from tkinter import *
 from PIL import Image
 import random, math, copy, time, pygame
+from datetime import date
+from openpyxl import Workbook, load_workbook
+
+'''
+Documentation for Workbooks on openpyxl: https://openpyxl.readthedocs.io/en/stable/tutorial.html
+
+
+######################## Worksheet documentation ##############################
+Create new workbook -> wb = Workbook()
+
+Get first sheet -> ws = wb.active
+
+To create a new sheet on workbook -> ws2 = wb.create_sheet(title = "Mysheet", index)
+By default create_sheet will create it at the end if no index is specified.
+Index 0 is the first position and index -1 is the penultimate
+
+To change title of a sheet -> ws.title = "New Title"
+
+Sheets can be obtained from workbook with their name as key -> ws3 = wb["New Title"]
+
+To see list of all of the sheets in a workbook -> print(wb.sheetnames)
+
+You can also loop through worksheets -> for sheet in wb:
+
+
+########################## Cells documentation ################################
+
+When a worksheet is created it contains no cells, they must be created first
+
+
+Cells can be accessed as keys on the ws -> c = ws['A4']
+
+Cells can also be accessed by cells using row and col -> d = ws.cell(row=4, column=2, value=10)
+
+Ranges of cells can be obtained through slicing -> cell_range = ws['A1':'C2']
+Example for columns: colC = ws['C'], col_range = ws['C:D']
+Example for rows: row10 = ws[10], row_range = ws[5:10]
+
+To iterate through all of the rows in a file -> ws.rows
+
+We can assign values to cells directly -> c.value = 'hello, world'
+
+##########################  Working with Workbooks #############################
+
+To save the current Workbook -> wb.save('balances.xlsx') 
+This will overwrite any existing workbook with the given name.
+
+To load up a workbook -> wb2 = load_workbook('test.xlsx')
+
+
+############################ Tricks and Tips #################################
+
+Set date and time using Python date -> ws['A1'] = datetime.datetime(2010, 7, 21)
+
+Adding a formula to a cell -> ws["A1"] = "=SUM(1, 1)"
+
+
+
+'''
 pygame.mixer.init()
 
 class EntryScreen(Mode):
@@ -18,7 +77,24 @@ class EntryScreen(Mode):
         pass
 
 class initializeSystem(Mode):
+    def getDate(mode):
+        today = date.today()
+        month, day = today.strftime("%m"), today.strftime("%d")
+        print(month+"-"+day)
+        return month+"-"+day
+
+    def getSheet(mode, path):
+        workBook = load_workbook(path)
+        newSheet = workBook.create_sheet(title = mode.getDate())
+        return (workBook, newSheet)
+
+    def getCatalog(mode, path):
+        return None
+
     def appStarted(mode):
+        mode.catalog = mode.getCatalog("catalog.txt")
+        mode.workBook, mode.sells = mode.getSheet("sellsLog.xlsx")
+
         pass
 
     def keyPressed(mode, event):
@@ -99,30 +175,22 @@ class Checkout(Mode):
     def redrawAll(mode, canvas):
         pass
 
-class Player(Mode):
-    def __init__(self, mode):
-        self.spriteCount, self.cx, self.cy = 0, 100, mode.height-178
-        self.lives, self.action, self.status = 2, 'standing', None
-        self.position, self.health, self.special = 'R', 100, 100
-        self.previousMove, self.storage, self.moves = None, list(), dict()
-
 class Product(object):
-    def __init__(self, kind, cx, cy):
-        self.kind, self.cx, self.cy = kind, cx, cy
-        if self.cx == 0:
-            self.position = 'L'
-        else:
-            self.position = 'R'
-        self.cooldown = 0
+    def __init__(self, kind):
+        pass
 
-class Beverages(object):
+class Beverage(object):
     def __init__(self, kind, cx, cy):
-        super().__init__(kind, cx, cy)
-        self.health, self.gotHit, self.hit, self.speed = 100, 0, False, 0
+        pass
 
 class Transaction(object):
-    def __init__(self, name, cx, cy):
-        self.name, self.cx, self.cy = name, cx, cy
+    """
+    To get the time for each transaction we do time = now.strftime("%H:%M:%S")
+print("time:", time)
+    
+    """
+    def __init__(self):
+        pass
 
 ### Mode superclass has been inherited from cmu_112_graphics 
 ### http://www.cs.cmu.edu/~112/notes/hw11.html
