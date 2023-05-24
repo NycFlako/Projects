fb8e878de750b01ad093b076e3ea9e965b47c209
Flako <54653591+Daniells-cmu@users.noreply.github.com>
2023-05-24 02:32:54 -0400

    Adding BookKeeping Project

---
+++ Bookeeping/main.go
@@ -0,0 +1,16 @@
+/*
+Documentation for excel sheets:
+https://xuri.me/excelize/en/workbook.html#NewFile
+
+Documentations for the graphics:
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name := "Flako"
+	fmt.Println("Developer: ", name)
+}
